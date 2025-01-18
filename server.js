68a3edcb76dd230eb6beeb32648b44b13634ddde
Andre Dos Reis <andredosreis@gmail.com>
2025-01-18 18:58:48 +0000

    finish thw first time this execese

--- server.js
+++ server.js
@@ -3,17 +3,39 @@
  * application. It is used to control the project.
  *******************************************/
 /* ***********************
+
  * Require Statements
  *************************/
+require("dotenv").config({ path: "./.env.sample" });
+console.log("PORT:", process.env.PORT);
+console.log("HOST:", process.env.HOST);
+
+
+
 const express = require("express")
+const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+
+
+/* ***********************
+ * View Engine and template
+ *************************/
+app.set("view engine", "ejs")
+app.use(expressLayouts)
+app.set("layout", "layouts/layout")//not at  views layout
+
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+
+// index route
+app.get("/", (req, res) => {
+  res.render("partials/index", { title: "Home" })
+})
 
 /* ***********************
  * Local Server Information
@@ -22,9 +44,12 @@ app.use(static)
 const port = process.env.PORT
 const host = process.env.HOST
 
+
+
+
 /* ***********************
  * Log statement to confirm server operation
  *************************/
-app.listen(port, () => {
-  console.log(`app listening on ${host}:${port}`)
-})
+app.listen(port, host, () => {
+  console.log(`App listening on ${host}:${port}`);
+});
