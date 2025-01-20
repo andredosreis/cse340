797a3e2088570bda6418f4ba6933af1ba2734f67
Andre Dos Reis <andredosreis@gmail.com>
2025-01-20 19:37:32 +0000

    mudança para render funcionar

--- server.js
+++ server.js
@@ -41,8 +41,8 @@ app.get("/", (req, res) => {
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500;
+const host = process.env.HOST || '0.0.0.0';
 
 
 
