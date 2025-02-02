044ff95d48f047aea66988a26d81c7d6110c9ca4
Andre Dos Reis <andredosreis@gmail.com>
2025-02-02 00:42:02 +0000

    atualizando banco de dados e suas dependencias, corrigindo o env

--- database/assignment2.sql
+++ database/assignment2.sql
@@ -1,9 +1,6 @@
-SELECT * FROM public.classification ORDER BY classification_name;
-
-
-
 CREATE TYPE public.account_type AS ENUM
     ('Client', 'Employee', 'Admin');
+
 
 
 -- Table structure for table 'classification'
@@ -238,3 +235,36 @@ VALUES   (
     'White',
     5
   );
+
+INSERT INTO public.account (account_firstname, account_lastname, account_email, account_password )
+VALUES ('Tony', 'Stark', 'tony@starkent.com', 'Iam1ronM@n');
+
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Tony';
+
+DELETE FROM account
+WHERE account_id = 1;
+
+UPDATE public.inventory
+SET inv_description = REPLACE(inv_description, 'small interiors','a huge interior')
+WHERE inv_id = 10;
+
+SELECT inv_make, inv_model, classification_name
+FROM public.inventory
+INNER JOIN public.classification
+ON inv_model = classification_name;
+
+UPDATE public.inventory
+SET inv_image = REPLACE (inv_image, '/images/', '/images/vehicles/'),
+inv_thumbnail = REPLACE (inv_thumbnail, '/images/', '/images/vehicles/');
+
+UPDATE account
+SET account_type = 'Admin'
+WHERE account_firstname = 'Manager';
+
+DELETE FROM classification
+WHERE classification_id = 6;
+
+DELETE FROM classification
+WHERE classification_id = 7;
