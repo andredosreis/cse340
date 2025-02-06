8a26abf9148036b415f2fbc27eecf0e6b16816cd
Andre Dos Reis <andredosreis@gmail.com>
2025-02-06 20:48:50 +0000

    corrigindo o menssagen e o css

--- controllers/baseController.js
+++ controllers/baseController.js
@@ -13,20 +13,3 @@ baseController.buildHome = async function(req, res){
 
 module.exports = baseController
 
-const { buildClassificationGrid } = require('../utilities');
-const VehicleModel = require('../models/vehicleModel');
-
-async function renderClassificationView(req, res) {
-  const classification = req.params.classification;
-  try {
-    const data = await VehicleModel.getVehiclesByClassification(classification);
-    const grid = buildClassificationGrid(data); // Gera o HTML da grid
-    const title = `${classification} Vehicles`;
-    res.render('inventory/classification', { title, grid });
-  } catch (error) {
-    console.error('Error rendering classification view:', error);
-    res.status(500).send('Internal Server Error');
-  }
-}
-
-module.exports = { renderClassificationView };
